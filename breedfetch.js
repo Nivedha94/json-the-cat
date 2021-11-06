c8b950800d8b328dda9bf4c4a804e978cb12aded
vagrant <nivedha.selvan@gmail.com>
2021-11-06 16:34:34 +0000

    cats refactoring

--- breedfetch.js
+++ breedfetch.js
@@ -1,16 +1,19 @@
 
 const request = require("request");
 
-const args = process.argv.slice(2);
-let breed = args[0];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  if (error) {
-    console.log("error")
-  }
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      console.log("error");
+    }
  
-  const data = JSON.parse(body);
-  console.log(data);
+    const data = JSON.parse(body);
+    console.log(data);
 
-})
+  });
+};
+
+module.exports = { fetchBreedDescription };
